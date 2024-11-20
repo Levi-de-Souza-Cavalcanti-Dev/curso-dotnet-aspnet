1d709ea96f17e7c9e9cd87ca1345df12652dc494
Levi <126890196+Levi-de-Souza-Cavalcanti-Dev@users.noreply.github.com>
2024-11-20 15:44:15 -0300

    Create Repository Interface
    Create Repository Interface

--- src/Api.Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
+++ src/Api.Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bc8f9a4ec54e619e8a6793a1d5771371682603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f18c346049cea65fa4b950b4197893ba5f0462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
