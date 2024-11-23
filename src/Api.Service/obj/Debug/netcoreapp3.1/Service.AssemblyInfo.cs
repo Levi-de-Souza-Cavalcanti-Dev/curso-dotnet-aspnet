78166f476c3bc31aa94d00c9a3e89ab21137c2cc
Levi <126890196+Levi-de-Souza-Cavalcanti-Dev@users.noreply.github.com>
2024-11-23 20:22:47 -0300

    Create references
    Create references from Service Layer to Api.Domain, Api.Data and Api.CrossCutting

--- src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
+++ src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7efa2b9b3911e9be80af06ac7d41c23f1df0d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5b62f61da6c570c2404fdf9ec0f7c3054e90a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
